716dab2e12e6964f4eb93987c63c0a99f278f333
Massi <terzimassimiliano@gmail.com>
2018-02-03 20:02:30 +0000

    Added geolocation and tests

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message.js');
+var {generateMessage, generateLocationMessage} = require('./message.js');
 
 describe('generateMessage', () => {
 	it('should generate correct message object', () => {
@@ -11,5 +11,19 @@ describe('generateMessage', () => {
 		expect(typeof(message.createdAt)).toEqual('number');
 		expect(message.from).toBeTruthy();
 		expect(message.text).toBeTruthy();	
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		var from = 'Deb';
+		var latitude = 15;
+		var longitude = 19;
+		var url = 'https://www.google.com/maps?q=15,19';
+		var message = generateLocationMessage(from, latitude, longitude);
+
+		expect(typeof(message.createdAt)).toEqual('number');
+		expect(message.url).toEqual(url);
+
 	});
 });
