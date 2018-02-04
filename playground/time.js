9b2155dc45cfc81d18f9d7f90b81143e102c72e0
Massi <terzimassimiliano@gmail.com>
2018-02-04 08:35:10 +0000

    Added moment library

---
+++ playground/time.js
@@ -0,0 +1,14 @@
+var moment = require('moment');
+
+// var date = new Date();
+
+// var months = ['Jan', 'Feb'];
+// console.log(date.getMonth());
+
+// var date = moment();
+// date.add(1, 'years').subtract(9, 'months');
+// console.log(date.format('MMM Do, YYYY'));
+
+var date = moment();
+
+console.log(date.format('h:mm a'));
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,10 +1,12 @@
 /*jshint esversion: 6 */
 
+var moment = require('moment');
+
 var generateMessage = (from, text) => {
 	return {
 		from,
 		text, 
-		createdAt: new Date().getTime()
+		createdAt: moment().valueOf()
 	};
 };
 
@@ -12,7 +14,7 @@ var generateLocationMessage = (from, latitude, longitude) => {
 	return {
 		from, 
 		url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-		createdAt: new Date().getTime()
+		createdAt: moment().valueOf()
 	};
 };
 
